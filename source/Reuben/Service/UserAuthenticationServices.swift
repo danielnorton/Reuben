4accdebcc3da3735f3bd5e138618610c570cec09
Daniel Norton <daniel.norton@brimstead.com>
2015-08-11 01:39:37 -0500

    begin user auth services

---
+++ source/Reuben/Service/UserAuthenticationServices.swift
@@ -0,0 +1,53 @@
+//
+//  UserAuthenticationServices.swift
+//  Reuben
+//
+//  Created by Daniel Norton on 8/11/15.
+//  Copyright © 2015 Daniel Norton. All rights reserved.
+//
+
+import UIKit
+
+class UserAuthenticationServices {
+    
+    let securityServiceName: String
+    internal static let tokenName = "Reuben User OAuth Token"
+    
+    
+    // MARK: -
+    // MARK: UserAuthenticationServices
+    init(_ securityServiceName: String) {
+        
+        self.securityServiceName = securityServiceName
+    }
+    
+    
+    // MARK: Public Functions
+    var hasUser: Bool {
+
+        get {
+            
+            var answer = false
+            
+            let service = CredentialStore.self
+            service.find(UserAuthenticationServices.tokenName
+                , serviceName: securityServiceName
+                , attributes: true
+                , foundAction: { (_) -> Void in
+
+                    answer = true
+                    
+                }, notFoundAction: nil)
+            
+            return answer
+        }
+    }
+    
+    func logout() {
+        
+        if !self.hasUser { return }
+        
+        let service = CredentialStore.self
+        service.remove(UserAuthenticationServices.tokenName, serviceName: securityServiceName)
+    }
+}
---
+++ source/ReubenTests/UserAuthenticationServicesTests.swift
@@ -0,0 +1,51 @@
+//
+//  UserAuthenticationServicesTests.swift
+//  Reuben
+//
+//  Created by Daniel Norton on 8/11/15.
+//  Copyright © 2015 Daniel Norton. All rights reserved.
+//
+
+import XCTest
+@testable import Reuben
+
+class UserAuthenticationServicesTests: XCTestCase {
+    
+    let noAccountService = "\(self.self)_noAccountService"
+    let hasAccountService = "\(self.self)_hasAccountService"
+    let user = UserAuthenticationServices.tokenName
+    let password = "password"
+    
+    override func setUp() {
+        
+        let service = CredentialStore.self
+        service.save(user, serviceName: hasAccountService, password: password)
+        service.remove(user, serviceName: noAccountService)
+    }
+    
+    override func tearDown() {
+        
+        let service = CredentialStore.self
+        service.remove(user, serviceName: hasAccountService)
+        service.remove(user, serviceName: noAccountService)
+    }
+    
+    func testHasAccount() {
+        
+        let service = UserAuthenticationServices(hasAccountService)
+        XCTAssert(service.hasUser)
+    }
+    
+    func testDoesntHaveAccount() {
+        
+        let service = UserAuthenticationServices(noAccountService)
+        XCTAssertFalse(service.hasUser)
+    }
+    
+    func testLogout() {
+        
+        let service = UserAuthenticationServices(hasAccountService)
+        service.logout()
+        XCTAssertFalse(service.hasUser)
+    }
+}
